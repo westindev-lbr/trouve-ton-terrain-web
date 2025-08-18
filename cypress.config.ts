e1017f340a96d5af20ebcda41ce71192760d4fbe
Axel Labarre <axel.labarre@gmail.com>
2025-08-19 00:36:54 +0200

    feat: add Cypress integration for end-to-end testing in CI workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    baseUrl: process.env['CI_BASE_URL'] || 'http://localhost:4200',
+    video: false,
+    retries: { runMode: 2, openMode: 0 },
   },
-  
-  
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-  
-})
+    specPattern: '**/*.cy.ts',
+  },
+});
